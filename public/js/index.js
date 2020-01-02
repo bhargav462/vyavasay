b07a753918eec712d9bcda488ddbc2f7f72fb8a9
Nakkina Bhargav <43701429+bhargav462@users.noreply.github.com>
2020-01-02 16:56:27 +0530

    new index

--- public/js/index.js
+++ public/js/index.js
@@ -3,12 +3,16 @@
     });
     
     document.getElementById("Register").addEventListener("click",function(){
-        document.querySelector(".popup").style.display = "flex";
+        document.querySelector(".popup-reg").style.display = "flex";
     });
     
     document.querySelector(".close").addEventListener("click",function(){
        document.querySelector(".popup").style.display = "none"; 
     });
+
+    document.querySelector(".close-reg").addEventListener("click",function(){
+        document.querySelector(".popup-reg").style.display = "none"; 
+     });
 
 
 // function closePopUp() {
